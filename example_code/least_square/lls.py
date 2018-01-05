0f0af5ee2d5e184449a353b4a86ad4354cf81c1d
hongzhouye <hzye2011@mit.edu>
2018-01-04 21:17:38 -0500

    adds example code lls.py

---
+++ example_code/least_square/lls.py
@@ -0,0 +1,65 @@
+"""
+This script provides functions for linear least square fitting
+"""
+
+
+import numpy as np
+import sys
+from matplotlib import pyplot
+
+
+def lls_fit(X, Y):
+    """Given X and Y, solve A that optimizes the 2-norm difference between
+    A.T @ X and Y.
+
+    Attributes:
+        X (np.ndarray):
+            Dimension: n-by-N. X = [x^1, x^2, \cdots{}, x^N] where x^i is the
+            i-th data point vector.
+        Y (np.ndarray):
+            Dimension: m-by-N. Y = [y^1, y^2, \cdots{}, y^N] where y^i is the
+            i-th target vector.
+
+    Returns:
+        A (np.ndarray):
+            Dimension: n-by-m.
+    """
+    if X.ndim == 1:
+        A = X@Y / (X@X)
+    else:
+        A = np.linalg.pinv(X@X.T) @ X@Y.T
+    return A
+
+
+def lls_predict(A, X):
+    """Given X and A, compute predicted value Y = A.T@X
+    """
+    if X.ndim == 1:
+        return A * X
+    else:
+        return A.T @ X
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: dataX dataY")
+        sys.exit(1)
+
+    # read data
+    X = np.loadtxt(sys.argv[1])
+    Y = np.loadtxt(sys.argv[2])
+
+    # put data into the desired format
+    X = X.T
+    Y = Y.T
+
+    # fit
+    A = lls_fit(X, Y)
+
+    # predict
+    pred_Y = lls_predict(A, X)
+
+    # plot and compare
+    pyplot.plot(X, Y, 'ko')
+    pyplot.plot(X, pred_Y, 'rs')
+    pyplot.show()
